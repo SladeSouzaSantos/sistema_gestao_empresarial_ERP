e2dee474b5889db6a3ab0ac1d60b09b8261d0a99
Pedro Santos <phassantos.dev@gmail.com>
2025-11-17 00:14:58 -0300

    Criação das funcionalidades da API, relacionadas as Tasks dos funcionários

--- companies/urls.py
+++ companies/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from companies.views.employees import Employees, EmployeeDetail
 from companies.views.permissions import PermissionDetail
 from companies.views.groups import Groups, GroupDetail
+from companies.views.tasks import Tasks, TaskDetail
 
 urlpatterns = [
     # Employees Endpoints
@@ -13,4 +14,9 @@ urlpatterns = [
     path("groups", Groups.as_view()),
     path("groups/<int:group_id>", GroupDetail.as_view()),
     path('permissions', PermissionDetail.as_view()),
+
+    # Tasks Endpoints
+    path('tasks', Tasks.as_view()),
+    path('tasks/<int:task_id>', TaskDetail.as_view()),
+    
 ]
